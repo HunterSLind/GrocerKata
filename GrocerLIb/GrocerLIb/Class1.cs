986a9ef0266442e5a9ac8bd020767338cee84596
Hunter Lind <hunterslind@gmail.com>
2019-02-28 17:54:13 -0600

    Initial push (outside of git ignore)

---
+++ GrocerLIb/GrocerLIb/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GrocerLIb
+{
+    public class Class1
+    {
+    }
+}
---
+++ GrocerLIb/GrocerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GrocerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GrocerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c86eeedb-903d-4daf-8247-93f13e25dbeb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
