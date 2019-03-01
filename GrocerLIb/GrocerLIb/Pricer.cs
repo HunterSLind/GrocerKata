31e82552a1fa8dc0286af568c950164a8e0159f3
Hunter Lind <hunterslind@gmail.com>
2019-02-28 18:27:29 -0600

    Update pricer class, no longer static. Added new method CheckPrice(String arg) -- Currently has red test.

--- GrocerLIb/GrocerLIb/Pricer.cs
+++ GrocerLIb/GrocerLIb/Pricer.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GrocerLib
 {
-    public static class Pricer
+    public class Pricer
     {
         public static decimal CheckPrice()
         {
-            return 1.99M;
+                return 1.99M;
+        }
+
+        public decimal CheckPrice(string itemName)
+        {
+            switch(itemName)
+            {
+                default:
+                    return 0.00M;
+            }
         }
     }
 }
--- GrocerLIb/GrocerTests/GrocerTests.cs
+++ GrocerLIb/GrocerTests/GrocerTests.cs
@@ -7,10 +7,18 @@ namespace GrocerTests
     [TestClass]
     public class GrocerTests
     {
+
+        Pricer pricer = new Pricer();
         [TestMethod]
         public void TestPriceCheck()
         {
             Assert.AreEqual(1.99M, Pricer.CheckPrice());
+        }
+
+        [TestMethod]
+        public void TestItemPriceByName()
+        {
+            Assert.AreEqual(1.99M, pricer.CheckPrice("HotDog"));
         }
     }
 }
