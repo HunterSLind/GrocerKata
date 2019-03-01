df22ae08fcab9571753aa87cf3c4b8667161235b
Hunter Lind <hunterslind@gmail.com>
2019-02-28 18:58:25 -0600

    Remvoed code that I did not have coverage for.

--- GrocerLIb/GrocerLIb/Pricer.cs
+++ GrocerLIb/GrocerLIb/Pricer.cs
@@ -7,18 +7,17 @@ namespace GrocerLib
     {
         public static decimal CheckPrice()
         {
-                return 1.99M;
+            return 1.99M;
         }
 
         public decimal CheckPrice(string itemName)
         {
-            switch(itemName)
-            {
-                case "HotDog":
-                    return 1.99M;
-                default:
-                    return 0.00M;
-            }
+            return itemPriceDictionary[itemName];
         }
+
+        private Dictionary<string, decimal> itemPriceDictionary = new Dictionary<string, decimal>
+        {
+            {"HotDog", 1.99M }
+        };
     }
 }
